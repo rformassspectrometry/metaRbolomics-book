1a185ffb98034ad12a039b3a4f429aacff5bc467
stanstrup <stanstrup@gmail.com>
2019-09-08 20:48:03 +0200

    first test

---
+++ .gitlab-ci.R
@@ -0,0 +1,19 @@
+sessionInfo()
+
+# Install blogdown --------------------------------------------------------
+r <- getOption("repos")
+  r["CRAN"] <- "http://cran.rstudio.com"
+  options(repos = r)
+
+install.packages("readr")
+install.packages("DT")
+
+    
+install.packages(c('xlsx'))
+install.packages("nlme")
+install.packages("lme4")
+install.packages("multcomp")
+install.packages("reshape2")
+install.packages("plyr")
+install.packages("knitr")
+install.packages("png")
