a4c54d9d973a4eb90eec861b7ae16cc2fd7bc40f
stanstrup <stanstrup@gmail.com>
2019-09-08 20:52:03 +0200

    removed not needed deps

--- .gitlab-ci.R
+++ .gitlab-ci.R
@@ -7,13 +7,3 @@ r <- getOption("repos")
 
 install.packages("readr")
 install.packages("DT")
-
-    
-install.packages(c('xlsx'))
-install.packages("nlme")
-install.packages("lme4")
-install.packages("multcomp")
-install.packages("reshape2")
-install.packages("plyr")
-install.packages("knitr")
-install.packages("png")
