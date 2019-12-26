19002af35297f7a6403f603e2529cfbcc80d0998
Steffen Neumann <sneumann@ipb-halle.de>
2019-12-26 18:16:33 +0100

    Configurable output directory

--- buildbook.sh
+++ buildbook.sh
@@ -7,4 +7,8 @@ if [ -z "$RENDER_BOOK_TARGET" ] ; then
   RENDER_BOOK_TARGET='c("bookdown::gitbook", "bookdown::pdf_book", "bookdown::epub_book")'
 fi 
 
-Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET')'
+if [ -z "$OUTPUT_DIR" ] ; then 
+  OUTPUT_DIR=NULL
+fi 
+
+Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET', output_dir='$OUTPUT_DIR')'
