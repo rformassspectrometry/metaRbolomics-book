8e542ba8c8353df91e9e6c23986351eaeaf5847c
Steffen Neumann <sneumann@ipb-halle.de>
2019-12-26 17:34:31 +0100

    Add environment variables for Travis matrix build

--- buildbook.sh
+++ buildbook.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 git clone --depth 1 https://github.com/rformassspectrometry/metaRbolomics-book.git
 cd metaRbolomics-book
-Rscript -e 'bookdown::render_book("index.Rmd", c("bookdown::gitbook", "bookdown::pdf_book", "bookdown::epub_book"))'
+
+if [ -z "$RENDER_BOOK_TARGET" ] ; then 
+  RENDER_BOOK_TARGET='c("bookdown::gitbook", "bookdown::pdf_book", "bookdown::epub_book")'
+fi 
+
+Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET')'
