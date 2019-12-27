7a882fe438938a049c31c520371d99657a9462c4
Steffen Neumann <sneumann@ipb-halle.de>
2019-12-27 22:20:07 +0100

    debug on travis

--- buildbook.sh
+++ buildbook.sh
@@ -4,4 +4,7 @@ if [ -z "$RENDER_BOOK_TARGET" ] ; then
   RENDER_BOOK_TARGET='c("bookdown::gitbook", "bookdown::pdf_book", "bookdown::epub_book")'
 fi 
 
+cd /tmp/metaRbolomics-book
+ls -l >/dev/stderr
+
 Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET')'
