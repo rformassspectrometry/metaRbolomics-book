5cb60dce0cc7639d2b82a3af91cfd9ae4cb77c44
Steffen Neumann <sneumann@ipb-halle.de>
2019-12-27 09:34:43 +0100

    create output directory

--- runDockerizedBuild.sh
+++ runDockerizedBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir public
 chmod -R 777 $PWD/public
 docker run -v $PWD/public:/tmp/public -e OUTPUT_DIR='"/tmp/public"' -e RENDER_BOOK_TARGET sneumann/metarbolomics-book-base /buildbook.sh
  
