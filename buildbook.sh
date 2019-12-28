3a9e588b393a069a09ff25d18423538688f4ebbb
Steffen Neumann <sneumann@ipb-halle.de>
2019-12-28 16:58:05 +0100

    better quoting

--- buildbook.sh
+++ buildbook.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -x 
 
 if [ -z "$RENDER_BOOK_TARGET" ] ; then 
   RENDER_BOOK_TARGET='c("bookdown::gitbook", "bookdown::pdf_book", "bookdown::epub_book")'
@@ -6,5 +6,4 @@ fi
 
 cd /tmp/metaRbolomics-book
 
-echo Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET')' >/dev/stderr
-Rscript -e 'bookdown::render_book("index.Rmd",'$RENDER_BOOK_TARGET')'
+Rscript -e "bookdown::render_book('index.Rmd',${RENDER_BOOK_TARGET})"
--- travisScript.sh
+++ travisScript.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -x 
 
 chmod -R 777 $PWD
-
-echo docker run -v $PWD/:/tmp/metaRbolomics-book -e RENDER_BOOK_TARGET sneumann/metarbolomics-book-base /tmp/metaRbolomics-book/buildbook.sh >/dev/stdderr
 docker run -v $PWD/:/tmp/metaRbolomics-book -e RENDER_BOOK_TARGET sneumann/metarbolomics-book-base /tmp/metaRbolomics-book/buildbook.sh
